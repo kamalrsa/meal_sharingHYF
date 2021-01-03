84527ae8644654f5862b830665a2d233d02f5219
Kammal <kamalrsa@gmai.com>
2021-01-03 17:10:57 +0100

    added a meals

--- src/client/components/TestComponent/TestComponent.js
+++ src/client/components/TestComponent/TestComponent.js
@@ -1,13 +1,20 @@
 /* eslint-disable camelcase */
 /* eslint-disable @typescript-eslint/camelcase */
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import "./testComponentStyle.css";
 
 export default function TestComponent() {
+  const [data, setData] = useState("");
+  const fetchURL = "api/meals";
+  const getData = () => fetch(`${fetchURL}`).then((res) => res.json());
+  useEffect(() => {
+    getData().then((data) => setData(data));
+  }, []);
   return (
     <section className="test-component">
       <p>in a component</p>
+      {data?.map((e) => e.meals_name)}
     </section>
   );
 }
