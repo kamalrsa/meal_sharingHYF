8fb54f53aa693a81621d89850451c401d29dcc7f
Kammal <kamalrsa@gmai.com>
2021-01-16 12:00:16 +0100

    navbar

--- src/client/components/TestComponent/Navbar.js
+++ src/client/components/TestComponent/Navbar.js
@@ -4,54 +4,43 @@ import "bootstrap/dist/css/bootstrap.min.css";
 export default function Navbar() {
   return (
     <div>
-      <nav className="navbar navbar-dark navbar-expand-sm bg-primary fixed-top">
-        <div className="container">
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#Navbar"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <a className="navbar-brand" href="#">
-            MealSharingApp
-          </a>
-          <div className="collapse navbar-collapse" id="Navbar">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item">
-                <a className="nav-link" href="./home">
-                  <span className="fa fa-info"></span>Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="./meal-display">
-                  <span className="fa fa-info fa-lg"></span>Meal
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="add-meal">
-                  <span className="fa fa-address-card fa-lg"></span>AddMeal
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="reservation">
-                  <span className="fa fa-address-card fa-lg"></span>
-                  ViewReservation
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="give-reviews">
-                  <span className="fa fa-address-card fa-lg"></span>GiveReview
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="./contact">
-                  <span className="fa fa-address-card fa-lg"></span>Contact
-                </a>
-              </li>
-            </ul>
-          </div>
+      <nav className="navbar navbar-dark navbar-expand bg-primary fixed-top">
+        <a className="navbar-brand" href="#">
+          MealSharingApp
+        </a>
+        <div className="collapse navbar-collapse" id="Navbar">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="./home">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="./meal-display">
+                Meal
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="add-meal">
+                AddMeal
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="reservation">
+                ViewReservation
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="give-reviews">
+                GiveReview
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="./contact">
+                Contact
+              </a>
+            </li>
+          </ul>
         </div>
       </nav>
     </div>
