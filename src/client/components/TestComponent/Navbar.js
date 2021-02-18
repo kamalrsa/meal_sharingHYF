1e723759a95327d86ae885ee73b9232b4580507e
Kammal <kamalrsa@gmail.com>
2021-02-18 14:26:28 +0100

    changes in navbar

--- src/client/components/TestComponent/Navbar.js
+++ src/client/components/TestComponent/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { NavLink } from "react-router-dom";
+//import { Navbar } from "react-router-dom";
 
 export default function Navbar() {
   return (
@@ -15,41 +15,41 @@ export default function Navbar() {
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <NavLink className="navbar-brand" to="#">
+          <a className="navbar-brand" href="#">
             MealSharingApp
-          </NavLink>
+          </a>
           <div className="collapse navbar-collapse" id="Navbar">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item">
-                <NavLink className="nav-link" to="./home">
+                <a className="nav-link" href="./home">
                   <span className="fa fa-info"></span>Home
-                </NavLink>
+                </a>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link" to="./meal-display">
+                <a className="nav-link" href="./meal-display">
                   <span className="fa fa-info fa-lg"></span>Meal
-                </NavLink>
+                </a>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link" to="add-meal">
+                <a className="nav-link" href="add-meal">
                   <span className="fa fa-address-card fa-lg"></span>AddMeal
-                </NavLink>
+                </a>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link" to="reservation">
+                <a className="nav-link" href="reservation">
                   <span className="fa fa-address-card fa-lg"></span>
                   ViewReservation
-                </NavLink>
+                </a>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link" to="give-reviews">
+                <a className="nav-link" href="give-reviews">
                   <span className="fa fa-address-card fa-lg"></span>GiveReview
-                </NavLink>
+                </a>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link" to="./contact">
+                <a className="nav-link" href="./contact">
                   <span className="fa fa-address-card fa-lg"></span>Contact
-                </NavLink>
+                </a>
               </li>
             </ul>
           </div>
